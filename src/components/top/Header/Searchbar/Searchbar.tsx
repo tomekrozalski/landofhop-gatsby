1c77ea2fa3c695e246a6ffe2ad82a5bb366fa07c
Tomek Rozalski <alium@daucusetanser.pl>
2020-01-24 17:38:19 +0100

    [SearchBar] Start working on search call to back-end API

--- src/components/top/Header/Searchbar/Searchbar.tsx
+++ src/components/top/Header/Searchbar/Searchbar.tsx
@@ -5,11 +5,28 @@ import { Button, Input, Wrapper } from '.';
 const Searchbar = () => {
 	const [input, setInput] = useState('');
 
+	const onSearchChange = async (e: any) => {
+		if (e.target) {
+			const rawResults = await fetch('http://localhost:4000/beverage/search', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ phrase: e.target.value, language: "pl" })
+			});
+
+			const results = await rawResults.json();
+			console.log('results', results);
+
+			setInput(e.target.value);
+		}
+	}
+
 	return (
 		<Wrapper>
 			<Input
 				value={input}
-				onChange={(e) => setInput(e.target.value)}
+				onChange={onSearchChange}
 				onBlur={() => setInput('')}
 			/>
 			<Button />
