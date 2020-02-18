ec11d6b832dcb57ab9c68a3051250cc88b713c99
Tomek Rozalski <alium@daucusetanser.pl>
2020-02-18 17:30:19 +0100

    feature(threejs-gallery): First try of react-three-fiber. It is good, easier than PIXI.js because it has wider documentation and more examples in the web

--- src/components/BeverageDetails/Gallery/Gallery.tsx
+++ src/components/BeverageDetails/Gallery/Gallery.tsx
@@ -1,10 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef, useMemo, Suspense } from 'react';
 import styled from 'styled-components';
 import { useIntl } from 'gatsby-plugin-intl';
+import { LinearFilter, TextureLoader } from 'three';
+import { Canvas, useFrame, useLoader, useThree } from 'react-three-fiber';
 
 import { BeverageImageType } from 'utils/enums/beverage';
 import { BeverageContext } from 'components/BeverageDetails';
 import { CoverImage } from 'elements';
+
 import { BrokenContainer } from '.';
 
 const Wrapper = styled.aside`
@@ -14,7 +17,39 @@ const Wrapper = styled.aside`
 	align-items: center;
 	width: 220px;
 	margin: 0 auto;
+	position: relative;
 `;
+
+const Box = () => {
+	const { viewport, aspect } = useThree();
+
+	console.log('-->', viewport, aspect);
+
+	// This reference will give us direct access to the mesh
+	const mesh = useRef()
+
+	// Set up state for the hovered and active state
+	const [hovered, setHover] = useState(false)
+	const [active, setActive] = useState(false)
+
+	// Rotate mesh every frame, this is outside of React without overhead
+	useFrame(() => (mesh.current.rotation.z += 0.02))
+
+	const textureUrl = 'https://land-of-hop-images.s3.eu-central-1.amazonaws.com/browar-pinta/the-big-pig/d6uj6o/container/jpg/2x/01.jpg';
+	const texture = useLoader(TextureLoader, textureUrl);
+	useMemo(() => (texture.minFilter = LinearFilter), []);
+
+	return (
+		<mesh
+			ref={mesh}
+			scale={[viewport.width, viewport.height, 1]}
+		>
+
+			<planeBufferGeometry attach="geometry" />
+			<meshStandardMaterial attach="material" map={texture} depthTest={false} />
+		</mesh>
+	)
+}
 
 const Gallery: React.FC = () => {
 	const { formatMessage } = useIntl();
@@ -46,6 +81,15 @@ const Gallery: React.FC = () => {
 						alt={formatMessage({ id: 'errors.bevereageDetailsImageNotFound' })}
 					/>
 				)}
+			<div style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: -240, zIndex: 2 }}>
+				<Canvas orthographic={true}>
+					<Suspense fallback={() => <div>Load</div>}>
+						<ambientLight />
+						<Box />
+					</Suspense>
+				</Canvas>
+			</div>
+
 		</Wrapper>
 	);
 }
