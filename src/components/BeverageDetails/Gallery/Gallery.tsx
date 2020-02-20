b228378cab6e207d22e57a4a903f0d2b0b1a6645
Tomek Rozalski <alium@daucusetanser.pl>
2020-02-20 12:46:25 +0100

    feature(threejs-gallery): Gallery works, but it lags. Playing around

--- src/components/BeverageDetails/Gallery/Gallery.tsx
+++ src/components/BeverageDetails/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useRef, useMemo, Suspense } from 'react';
+import React, { useContext, useState, useRef, useMemo, useEffect, Suspense } from 'react';
 import styled from 'styled-components';
 import { useIntl } from 'gatsby-plugin-intl';
 import { LinearFilter, TextureLoader } from 'three';
@@ -20,33 +20,84 @@ const Wrapper = styled.aside`
 	position: relative;
 `;
 
-const Box = () => {
-	const { viewport, aspect } = useThree();
 
-	console.log('-->', viewport, aspect);
+const Image = ({ img, number }) => {
+	console.log('--h', img, number);
 
-	// This reference will give us direct access to the mesh
-	const mesh = useRef()
-
-	// Set up state for the hovered and active state
-	const [hovered, setHover] = useState(false)
-	const [active, setActive] = useState(false)
-
-	// Rotate mesh every frame, this is outside of React without overhead
-	useFrame(() => (mesh.current.rotation.z += 0.02))
-
-	const textureUrl = 'https://land-of-hop-images.s3.eu-central-1.amazonaws.com/browar-pinta/the-big-pig/d6uj6o/container/jpg/2x/01.jpg';
+	const textureUrl = `https://land-of-hop-images.s3.eu-central-1.amazonaws.com/browar-pinta/the-big-pig/d6uj6o/container/jpg/2x/${number}.jpg`;
 	const texture = useLoader(TextureLoader, textureUrl);
 	useMemo(() => (texture.minFilter = LinearFilter), []);
 
 	return (
-		<mesh
-			ref={mesh}
-			scale={[viewport.width, viewport.height, 1]}
-		>
-
+		<mesh visible={img === number}>
 			<planeBufferGeometry attach="geometry" />
-			<meshStandardMaterial attach="material" map={texture} depthTest={false} />
+			<meshStandardMaterial attach="material" map={texture} />
+		</mesh>
+	)
+}
+
+const GalleryContent = ({ badge, brand, shortId, photos }) => {
+	const { viewport } = useThree();
+
+	const [rotatable, setRotatable] = useState(false);
+	const [position, setPosition] = useState([0, 0]);
+	const [img, setImg] = useState('01');
+
+	useEffect(() => {
+		if (position[0] !== position[1]) {
+
+			const abs = +img - 1;
+			const parsed = position[0] < position[1] ? +abs - 1 : +abs + 1;
+			const aaa = parsed % 24;
+
+			let bbb = 0;
+
+			if (aaa < 0) {
+				bbb = 23 + aaa;
+			} else {
+				bbb = aaa;
+			}
+
+			const ccc = bbb + 1;
+			const normalized = ccc < 10 ? `0${ccc}` : ccc.toString();
+
+			setImg(normalized);
+		}
+	}, [position]);
+
+	const onMove = (e) => {
+		if (rotatable) {
+			setPosition(val => [e.clientX, val[0]])
+		}
+	}
+
+	const onWheelMove = (e) => {
+		console.log('e.clientY', e.deltaX, e.deltaY);
+
+		setPosition(val => [e.deltaX, val[0]])
+
+	}
+
+	const photosArr = useMemo(() => {
+		return new Array(photos).fill('').map((val, i) => {
+			const index = i + 1;
+
+			return index < 10 ? `0${index}` : index.toString();
+		})
+	}, []);
+
+
+	return (
+		<mesh
+			scale={[viewport.width, viewport.height, 1]}
+			onPointerUp={() => setRotatable(false)}
+			onPointerDown={() => setRotatable(true)}
+			onPointerOver={() => setRotatable(false)}
+			onPointerMove={onMove}
+			onWheel={onWheelMove}
+		>
+			<planeBufferGeometry attach="geometry" />
+			{photosArr.map(val => <Image key={val} img={img} number={val} />)}
 		</mesh>
 	)
 }
@@ -83,13 +134,12 @@ const Gallery: React.FC = () => {
 				)}
 			<div style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: -240, zIndex: 2 }}>
 				<Canvas orthographic={true}>
-					<Suspense fallback={() => <div>Load</div>}>
+					<Suspense fallback={'Loading'}>
 						<ambientLight />
-						<Box />
+						<GalleryContent badge="the-big-pig" shortId="d6uj6o" brand="browar-pinta" photos={24} />
 					</Suspense>
 				</Canvas>
 			</div>
-
 		</Wrapper>
 	);
 }
