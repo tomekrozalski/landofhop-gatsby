29e863c3bde0371d351fd2d7e112626a75a4e25d
Tomek Rozalski <alium@daucusetanser.pl>
2020-07-20 21:00:26 +0200

    [Configuration] Add Sentry.io support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,10 @@
 /* eslint-disable import/prefer-default-export, react/jsx-filename-extension */
 import React from 'react';
+import * as Sentry from '@sentry/browser';
 
 import GlobalStateProvider from './src/utils/contexts';
+
+Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 export const wrapRootElement = ({ element }) => (
   <GlobalStateProvider>{element}</GlobalStateProvider>
