405244e4c74fdd1c9c8a4aad1571c555e683a7b6
Tomek Rozalski <alium@daucusetanser.pl>
2020-08-21 20:08:18 +0200

    [Configuration] I had my iMac in renovation. Now I am back. Remove Immer and Sentry packages. I do not need them for now

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,7 @@
 /* eslint-disable import/prefer-default-export, react/jsx-filename-extension */
 import React from 'react';
-import * as Sentry from '@sentry/browser';
 
 import GlobalStateProvider from './src/utils/contexts';
-
-Sentry.init({ dsn: process.env.SENTRY_DSN });
 
 export const wrapRootElement = ({ element }) => (
   <GlobalStateProvider>{element}</GlobalStateProvider>
